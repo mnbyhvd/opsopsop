33a90ea3fdf7bb4a718f9423dcdd1acd9212788a
mnbyhvd <mnbyhvd@gmail.com>
2025-10-15 05:39:46 +0300

    12

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -33,7 +33,7 @@ const App: React.FC = () => {
   }
 
   return (
-    <Router basename="/admin">
+    <Router>
       <div className="min-h-screen">
         <Routes>
           <Route path="/*" element={<AdminDashboard onLogout={logout} />} />
